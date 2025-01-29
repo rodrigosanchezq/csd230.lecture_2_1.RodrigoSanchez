bf56d0bd9d90a6a1a69b3cab8685e6a11f349c91
Rodrigo Sanchez <23015753@saultcollege.ca>
2025-01-29 11:03:54 -0500

    Test coded

--- src/test/java/csd230/lecture_2_1/ApplicationTests.java
+++ src/test/java/csd230/lecture_2_1/ApplicationTests.java
@@ -1,13 +1,48 @@
 package csd230.lecture_2_1;
 
+import com.github.javafaker.Commerce;
+import com.github.javafaker.Faker;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Propagation;
 
-@SpringBootTest
-class ApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+
+public class ProductRepositoryTests {
+//	@Autowired
+//	private TestEntityManager entityManager;
+    @Autowired
+    private ProductRepository productRepository;
 
 	@Test
-	void contextLoads() {
+	void testSave() {
+		Faker faker = new Faker();
+		Commerce cm = faker.commerce();
+		com.github.javafaker.Number number = faker.number();
+		com.github.javafaker.Book fakeBook = faker.book();
+		String name = cm.productName();
+		String description = cm.material();
+		Product newProduct = new Product(name, description, number.randomDouble(2, 10, 100));
+
+		//testEM.persistAndFlush(b1);
+		productRepository.save(newProduct);
+
+		Long savedProductID = newProduct.getId();
+
+		Product product= productRepository.findById(savedProductID).orElseThrow();
+		//Product book = testEM.find(Product.class, savedProductID);
+
+		assertEquals(savedProductID, product.getId());
 	}
 
 }
